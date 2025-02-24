f18041e992975ab2a36b4be19cec5b0217fd82b9
Shakira Syevuo <shakirasyevuo@gmail.com>
2025-02-25 01:07:38 +0300

    Completed lab

---
+++ lib/create.sql
@@ -0,0 +1,10 @@
+CREATE TABLE bears (
+    --columns here
+    id INTEGER PRIMARY KEY,
+    name TEXT,
+    age INTEGER,
+    sex TEXT,
+    color TEXT,
+    temperament TEXT,
+    alive BLOB
+);
---
+++ lib/insert.sql
@@ -0,0 +1,9 @@
+INSERT INTO bears (name, age, sex, color, temperament, alive) VALUES 
+('Mr. Chocolate', 15, 'M', 'brown', 'gentle', 1),
+('Rowdy',  4, 'F', 'black', 'violent', 1),
+('Tabitha', 8, 'F', 'white', 'calm', 1),
+('Sergeant Brown', 12, 'M', 'brown', 'curious', 1),
+('Melissa', 3, 'F', 'white', 'gentle', 1),
+('Grinch', 5, 'M', 'black', 'grumpy', 1),
+('Wendy', 4, 'F', 'white', 'tolerant', 1),
+(NULL, 5, 'M', 'brown', 'peaceful', 1)
